7c6f843a0f9f2b253f3a3439bec286d791eb0b1d
Andrei <a.e.itiligator@gmail.com>
2020-11-16 15:33:40 +0700

    rollback settings, added l.txt as a working pip requirements

--- demo/demo/settings.py
+++ demo/demo/settings.py
@@ -176,7 +176,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            'hosts': [('host.docker.internal', 6379),],
+            'hosts': [('127.0.0.1', 6379),],
         },
     },
 }
